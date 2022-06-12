2c2229c0c4d6af2085ab0280c751b9ef0cc804b2
Максим Скибин <skibin-mvp@mail.ru>
2022-06-12 21:10:10 +0300

    dataset indexing functions

---
+++ dataset/indexing.py
@@ -0,0 +1,16 @@
+
+from typing import Tuple
+
+
+def get_index(folder_name: str) -> Tuple[str, str, str, str]:
+    split_name = folder_name.split(' ')
+    mark, model = split_name[0], split_name[1]
+    view = 'back' if 'back' in folder_name else 'normal'
+    truck = 'truck' if 'truck' in folder_name else 'no_truck'
+    index = (mark, model, view, truck)
+    return index
+
+
+def get_indexes(folders: list) -> list:
+    indexes = [get_index(fold_n) for fold_n in folders]
+    return indexes
