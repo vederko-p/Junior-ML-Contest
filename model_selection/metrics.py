66b7c16e1cb016b882f8d9c58f8285222c593b95
Максим Скибин <skibin-mvp@mail.ru>
2022-06-22 00:24:47 +0300

    docstrings + f1-score init

--- model_selection/metrics.py
+++ model_selection/metrics.py
@@ -1,22 +1,49 @@
 
 import torch
-import numpy as np
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from tqdm.notebook import tqdm
 
 
 class CustomAccuracy:
+    """Torch accuracy implementation"""
     def __init__(self):
         self.res_scope = None
         self.res_ds_scope = None
     
-    def scope(self, y_pred, y_true):
-        # y_pred, y_true - метки классов ~ torch.tensor
+    def scope(self, y_pred: torch.tensor, y_true: torch.tensor) -> float:
+        """Evaluate accuracy over tensor data.
+
+        Parameters
+        ----------
+        y_pred : `torch.tensor`
+            Class predictions by model.
+        y_true : `torch.tensor`
+            True classes.
+
+        Returns
+        -------
+        acc : `float`
+            Accuracy.
+        """
         res = torch.eq(y_pred, y_true).sum() / y_pred.shape[0]
         self.res_scope = res
         return res.item()
-    
-    def ds_scope(self, ds, model):
+
+    def ds_scope(self, ds: Dataset, model: torch.nn.Module):
+        """Evaluate accuracy over dataset.
+
+        Parameters
+        ----------
+        ds : `torch.utils.data.Dataset`
+            Classify dataset.
+        model : `torch.tensor`
+            Model.
+
+        Returns
+        -------
+        acc : `float`
+            Accuracy.
+        """
         l = len(ds)
         batch_size= 32
         dataloader = DataLoader(ds, batch_size=batch_size)
@@ -36,18 +63,47 @@ class CustomAccuracy:
 
 
 class CustomVarianceCriteria:
+    """Torch variance criteria implementation"""
     def __init__(self):
-        self.res = None
+        self.res_scope = None
         
-    def scope(self, X, y):
-        # X, y - torch.tensor; dtype=torch.float
+    def scope(self, x: torch.tensor, y: torch.tensor):
+        """Evaluate accuracy over tensor data.
+
+        Parameters
+        ----------
+        x : `torch.tensor`
+            Tensor of vectors with dtype=torch.float.
+        y : `torch.tensor`
+            Class label.
+
+        Returns
+        -------
+        acc : `float`
+            Accuracy.
+        """
         unique_lbls = torch.unique(y)
         size = unique_lbls.shape[0]
         groups_mean = torch.empty(size, dtype=torch.float)
         groups_var = torch.empty(size, dtype=torch.float)
-        for i,k in enumerate(unique_lbls):
-            groups_mean[i] = X[y==k].mean()
-            groups_var[i] = X[y==k].var()
+        for i, k in enumerate(unique_lbls):
+            groups_mean[i] = x[y == k].mean()
+            groups_var[i] = x[y == k].var()
         D_in = groups_var.mean().item()
         D_mg = groups_var.var().item()
-        return D_in / D_mg
+        crit_val = D_in / D_mg
+        self.res_scope = crit_val
+        return crit_val
+
+
+class F1Score:
+    """Torch f1 score implementation"""
+    def __init__(self):
+        self.res_scope = None
+        self.res_ds_scope = None
+
+    def scope(self):
+        pass
+
+    def ds_scope(self):
+        pass
