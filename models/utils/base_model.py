2d88a55ea49782a506bc95b19cdd803aa7faae77
Максим Скибин <skibin-mvp@mail.ru>
2022-06-24 17:30:55 +0300

    base model settings refactor

--- models/utils/base_model.py
+++ models/utils/base_model.py
@@ -6,11 +6,13 @@ import torch
 class BaseModel(nnModule):
     def __init__(self):
         super(BaseModel, self).__init__()
+        self.optimizer = None
+        self.criterion = None
+        self.device = None
 
-    def compile_settings(self, criterion, optimizer, device=None, log_module=None):
+    def compile_settings(self, criterion, optimizer, device=None):
         self.optimizer = optimizer
         self.criterion = criterion
-        self.loger = log_module  # loger не должен быть в compile, он должен передаваться через call back
         if device is None:
             if torch.cuda.is_available():
                 torch.cuda.empty_cache()
