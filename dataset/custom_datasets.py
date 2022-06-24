ca9785f9484d5752a29360ae41216a658d60d1e9
Максим Скибин <skibin-mvp@mail.ru>
2022-06-24 22:13:17 +0300

    triplet loss dataset

--- dataset/custom_datasets.py
+++ dataset/custom_datasets.py
@@ -1,6 +1,8 @@
 
 import os
-from typing import Tuple, Callable
+from typing import Tuple, Callable, List
+from collections import defaultdict
+from random import sample, choice
 
 import torch
 from torch.utils.data import Dataset
@@ -39,6 +41,17 @@ def get_imgs_paths(imgs_path: str):
     imgs = os.listdir(imgs_path)
     imgs_glob_paths = [os.path.join(imgs_path, img) for img in imgs]
     return imgs_glob_paths
+
+
+def collect_blocks(
+    images: List[str],
+    labels: List[tuple]
+) -> defaultdict:
+    """Split images into blocks by labels."""
+    blocks = defaultdict(list)
+    for img, lbl in zip(images, labels):
+        blocks[lbl].append(img)
+    return blocks
 
 
 class TLClassifyDataset(Dataset):
