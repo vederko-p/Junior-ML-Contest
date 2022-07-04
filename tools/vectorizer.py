6bef76e9d03d9ada203a30e48ad5aa30a847cf2b
Максим Скибин <skibin-mvp@mail.ru>
2022-07-04 22:28:49 +0300

    vectorize from path

--- tools/vectorizer.py
+++ tools/vectorizer.py
@@ -1,13 +1,19 @@
 
+from typing import List
+
 import torch
 from torch.utils.data import DataLoader
 from tqdm.notebook import tqdm
+
+import dataset.utils.image_process as img_proc
+from dataset import default_augmentations as def_augs
 
 
 class Vectorizer:
     def __init__(self):
         self.lables_res = None
         self.vects_res = None
+        self.vects_from_paths_res = None
 
     def vectorize(self, ds, model, batch_size: int = 32) -> None:
         model.eval()
@@ -22,3 +28,15 @@ class Vectorizer:
             vectors = torch.cat([vectors, vects])
         self.lables_res = labels
         self.vects_res = vectors
+
+    def vectorize_from_paths(self, imgs_paths: List[str], model) -> None:
+        model.eval()
+        vectors = torch.empty((0, model.n_out), dtype=torch.float)
+        for i, ip in enumerate(tqdm(imgs_paths)):
+            img_array = img_proc.open_image(ip)
+            img_array = def_augs.TL_no_aug_transform_128(image=img_array)['image']
+            img_tens = torch.permute(torch.FloatTensor(img_array), (2, 0, 1))
+            with torch.no_grad():
+                vect = model.forward(img_tens.unsqueeze(0))
+            vectors = torch.cat([vectors, vect.unsqueeze(0)])
+        self.vects_from_paths_res = vectors
