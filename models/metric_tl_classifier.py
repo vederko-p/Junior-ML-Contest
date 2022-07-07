f63e0797811c0f3766465c8efd0e47d8fd83b991
Максим Скибин <skibin-mvp@mail.ru>
2022-07-07 23:10:36 +0300

    mah models model view/truck generalization + models knn fix

--- models/metric_tl_classifier.py
+++ models/metric_tl_classifier.py
@@ -3,11 +3,25 @@ from typing import Tuple, List
 from random import sample
 
 import numpy as np
+from numpy.linalg import LinAlgError
 import pandas as pd
 import cv2 as cv
 from scipy.spatial.distance import mahalanobis
 
 from tools.feature_base import FeaturesBase
+
+
+def quasy_inv_matr(m, eps=10**(-10)):
+    U, S, V = np.linalg.svd(m)
+    dd = np.diag(np.diag(S))
+    S = np.diag(S)
+    dd2 = np.multiply(np.sign(dd), np.maximum(eps, np.abs(dd)))
+    dd_ = np.divide(1, dd2 + np.finfo(float).eps)
+    S_ = np.diag(dd_)
+    S1 = np.transpose(S)
+    S1[0:S_.shape[1]][0:S_.shape[0]] = S_
+    return np.matmul(np.matmul(V.T, S1), U.T)
+
 
 
 class MarkskNN:
@@ -68,8 +82,13 @@ class ModelskNN:
         else:
             marks_res = marks_res_0
         for i, mrk_id in enumerate(marks_res):
-            _, res, _, dist = self.knn[mrk_id].findNearest(
-                x[i:i + 1, -self.mdl_ftrs_len:], k_neighs)
+            try:
+                _, res, _, dist = self.knn[mrk_id].findNearest(
+                    x[i:i+1, -self.mdl_ftrs_len:], k_neighs)
+            except KeyError:
+                knn_key = list(filter(lambda u: u[0] == mrk_id[0], self.knn.keys()))[0]
+                _, res, _, dist = self.knn[knn_key].findNearest(
+                    x[i:i+1, -self.mdl_ftrs_len:], k_neighs)
             models_res.append(res[0, 0])
             models_dist.append(dist[0, 0])
         return np.array(models_res, dtype=np.int32), np.array(models_dist)
@@ -100,7 +119,7 @@ class MarksMah:
         dists_res = []
         if not self.keys:
             for i in range(x.shape[0]):
-                best_mark = None
+                best_mark = 1
                 best_dist = 1e10
                 for mrk_id, (centr, cov_inv) in self.mah.items():
                     dist = mahalanobis(x[i:i + 1, :self.ftrs_len], centr, cov_inv)
@@ -111,7 +130,7 @@ class MarksMah:
                 dists_res.append(best_dist)
         else:
             for i in range(x.shape[0]):
-                best_mark = None
+                best_mark = 1
                 best_dist = 1e10
                 for ks, (centr, cov_inv) in self.mah.items():
                     mrk_id = ks[0]
@@ -139,41 +158,67 @@ class MarksMah:
 
 
 class ModelsMah:
-    def __init__(self, ftrs_base: FeaturesBase, lim: int = 1000):
+    def __init__(self, ftrs_base: FeaturesBase, lim: int = 1000,
+                 keys: List[str] = None):
         self.df_all = pd.DataFrame(ftrs_base.data_all)
+        self.keys = [] if keys is None else keys
         self.mrk_ftrs_len = 128
         self.mdl_ftrs_len = 64
         self.lim = lim  # max amount of points from class to take into account
         self.mah = {}  # {model_id: (mark_id, centroid, cov_inv)}
 
-    def forward(self, x, marks_res, k_neighs=15):
+    def forward(self, x, marks_res, k_neighs=15, keys: dict = None):
         models_res = []
         dists_res = []
-        for i in range(x.shape[0]):
-            best_model = None
-            best_dist = 1e10
-            for mdl_id, (mrk_id, centr, cov_inv) in self.mah.items():
-                if marks_res[i] == mrk_id:
-                    dist = mahalanobis(x[i:i+1, -self.mdl_ftrs_len:],
-                                       centr, cov_inv)
-                    if dist < best_dist:
-                        best_model = mdl_id
-                        best_dist = dist
-            models_res.append(best_model)
-            dists_res.append(best_dist)
+        if not self.keys:
+            for i in range(x.shape[0]):
+                best_model = None
+                best_dist = 1e10
+                for mdl_id, (mrk_id, centr, cov_inv) in self.mah.items():
+                    if marks_res[i] == mrk_id:
+                        dist = mahalanobis(x[i:i+1, -self.mdl_ftrs_len:],
+                                           centr, cov_inv)
+                        if dist < best_dist:
+                            best_model = mdl_id
+                            best_dist = dist
+                models_res.append(best_model)
+                dists_res.append(best_dist)
+        else:
+            for i in range(x.shape[0]):
+                best_model = 1
+                best_dist = 1e10
+                for ks, (mrk_id, centr, cov_inv) in self.mah.items():
+                    if marks_res[i] == mrk_id:
+                        mdl_id = ks[0]
+                        t = [keys[kn][i] == ksj for kn, ksj in zip(keys, ks[1:])]
+                        if sum(t):
+                            dist = mahalanobis(x[i:i + 1, :self.ftrs_len], centr, cov_inv)
+                            if dist < best_dist:
+                                best_model = mdl_id
+                                best_dist = dist
+                        else:
+                            continue
+                models_res.append(best_model)
+                dists_res.append(best_dist)
         return np.array(models_res), np.array(dists_res)
 
     def set_mah(self):
-        grouped = self.df_all.groupby(['ID_mark', 'ID_model'])
+        grouped = self.df_all.groupby(['ID_mark', 'ID_model'] + self.keys)
         for gk in grouped.groups:
             group = grouped.get_group(gk)
             rng = range(self.mrk_ftrs_len,
                         self.mrk_ftrs_len + self.mdl_ftrs_len)
             gX = group[[f'f_{i}' for i in rng]].values
             indxs = sample(range(gX.shape[0]), min(self.lim, gX.shape[0]))
+            if gX[indxs].shape[0] < 2:
+                continue
             centroid = gX[indxs].sum(axis=0)
-            cov_inv = np.linalg.inv(np.cov(gX[indxs], rowvar=False))
-            self.mah[gk[1]] = (gk[0], centroid, cov_inv)
+            cov_m = np.cov(gX[indxs], rowvar=False)
+            try:
+                cov_inv = np.linalg.inv(cov_m)
+            except LinAlgError:
+                cov_inv = quasy_inv_matr(cov_m)
+            self.mah[tuple([gk[1]] + self.keys)] = (gk[0], centroid, cov_inv)
 
 
 class MetricClassificationModel:
